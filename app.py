58e08a7ef9cff1dd3243da6dc8e4e30c6e4c8f8a
Santosh-2025aa05982 <2025aa05982@wilp.bits-pilani.ac.in>
2026-02-15 15:44:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import streamlit as st 
+import pandas as pd 
+import pickle 
+from sklearn.metrics import confusion_matrix, classification_report 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
 
+st.title("Student Performance Prediction Dashboard")
+upload_file - st.file_uploader("Upload Test Data (CSV)", type = "csv")
+
+if uploaded_file:
+  data = pd.read_csv(uploaded_file)
+  st.wrire("Data Preview:", data.head())
+
+#MFeature: Model selection
+model_option = st.selectbox(
+  "Choose a Classification Model",
+  ("logistic_regression", "decision_tree", "knn", "naive_bayes", "random_forest", 'xgboost")
+  )
+
+  #Load model 
+  with open(f'model/{model_options}.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+#Assuming data is preprocessed like the training set 
+if st.button("Run Evaluation"):
+  
